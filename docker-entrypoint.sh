d1d49ce336c372c27fb470196dd25954f8f941ed
defautstring <1075663501@qq.com>
2020-10-14 18:13:28 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,13 +17,13 @@ if [ -z $DISABLE_CRON ];then
     REFRESH_TOKEN=${REFRESH_TOKEN:-"0 * * * *"}
     REFRESH_CACHE=${REFRESH_CACHE:-"*/10 * * * *"}
     rm -rf /tmp/cron.`whoami`
-    echo "${REFRESH_TOKEN} php /var/www/html/one.php token:refresh" >> /tmp/cron.`whoami`
-    echo "${REFRESH_CACHE} php /var/www/html/one.php cache:refresh" >> /tmp/cron.`whoami`
+    echo "${REFRESH_TOKEN} php /var/www/html/oneindex/one.php token:refresh" >> /tmp/cron.`whoami`
+    echo "${REFRESH_CACHE} php /var/www/html/oneindex/one.php cache:refresh" >> /tmp/cron.`whoami`
     crontab -u `whoami` /tmp/cron.`whoami`
     crond
 fi
 
 sed -i "s|listen 80|listen ${PORT:-80}|" /etc/nginx/conf.d/default.conf
-chown -R www-data:www-data /var/www/html/cache
-chown -R www-data:www-data /var/www/html/config
+chown -R www-data:www-data /var/www/html/oneindex/cache
+chown -R www-data:www-data /var/www/html/oneindex/config
 php-fpm & nginx '-g daemon off;'
